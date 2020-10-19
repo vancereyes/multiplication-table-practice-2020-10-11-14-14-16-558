6a39192ecd07da6971bd8429bcd33abf6a8d03ee
VANCE ALBERT REYES (SHP-IRIS-ISD-OOCLL/MNL) <REYESVA@OOCL.COM>
2020-10-19 10:34:12 +0800

    [MultiplicationTable][Vance]feature - added print multiplication table

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -1,7 +1,40 @@
 package com.tw;
-
 public class MultiplicationTable {
-    public String create(int startNumber, int endNumber) {
+
+
+    public String createMultiplicationTable(int firstNum, int secondNum) {
+        boolean isValid = firstIsSmallerThanSecond(firstNum, secondNum);
+        boolean inRange = inRange1To100(firstNum, secondNum);
+        boolean isPassed = isValid && inRange;
+        return printMultiplicationTable(firstNum, secondNum, isPassed);
+    }
+    public boolean inRange1To100 ( int firstNum, int secondNum){
+        return (firstNum >= 1 && firstNum <= 1000) && (secondNum >= 1 && secondNum <= 1000);
+    }
+
+    public boolean firstIsSmallerThanSecond ( int firstNum, int secondNum){
+        return (firstNum <= secondNum);
+    }
+
+    public String printMultiplicationTable(int firstNum, int secondNum, boolean isPassed) {
+        if (isPassed) {
+            String table = "";
+
+            for (int a = firstNum; a <= secondNum; a++) {
+                for (int b = firstNum; b <= a; b++) {
+                    table += b + "*";
+                    table += a + "=";
+                    table += a * b;
+                    if (a != b) {
+                        table += "  ";
+                    }
+                }
+                table += "\r\n";
+            }
+            return table.trim();
+        }
         return null;
     }
+
+
 }
--- src/test/java/com/tw/MultiplicationTableTest.java
+++ src/test/java/com/tw/MultiplicationTableTest.java
@@ -14,7 +14,7 @@ public class MultiplicationTableTest {
         int end = 1;
 
         //when
-        String actual = multiplicationTable.create(start, end);
+        String actual = multiplicationTable.createMultiplicationTable(start, end);
 
         //then
         assertNull(actual);
@@ -28,12 +28,12 @@ public class MultiplicationTableTest {
         int end = 1001;
 
         //when
-        String actual = multiplicationTable.create(start, end);
+        String actual = multiplicationTable.createMultiplicationTable(start, end);
 
         //then
         assertNull(actual);
     }
-
+//
     @Test
     void should_return_single_equation_when_multiply_given_start_number_2_end_number_2() {
         //given
@@ -42,12 +42,12 @@ public class MultiplicationTableTest {
         int end = 2;
 
         //when
-        String actual = multiplicationTable.create(start, end);
+        String actual = multiplicationTable.createMultiplicationTable(start, end);
 
         //then
         assertEquals("2*2=4", actual);
     }
-
+//
     @Test
     void should_return_multiply_table_when_multiply_given_start_number_2_end_number_5() {
         //given
@@ -56,7 +56,7 @@ public class MultiplicationTableTest {
         int end = 5;
 
         //when
-        String actual = multiplicationTable.create(start, end);
+        String actual = multiplicationTable.createMultiplicationTable(start, end);
         String expected = String.format("2*2=4%n2*3=6  3*3=9%n2*4=8  3*4=12  4*4=16%n2*5=10  3*5=15  4*5=20  5*5=25");
 
         //then
